a1aa796ea8ef5b54228d6596a68bd7094e482ca3
Joshua Clarke <josh.clarke11@gmail.com>
2019-11-04 15:13:10 -0500

    created components and service

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# weather
+# Weather
+A Simple Weather App\
+CurrentVersion: 1.0.0
+
+## Description
+A simple weather app
+
+## Setup Instructions
+
+## Author
+Joshua Clarke
+
+## Github
+https://github.com/jlclarke13/weather#readme
+
+## Bugs
+Please feel free to report bugs, however this is not an LTS app.\
+https://github.com/jlclarke13/weather/issues
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './components/view/page-not-found/page-not-found.component';
+import { SearchComponent } from './components/view/search/search.component';
+import { WeatherComponent } from './services/weather/weather.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageNotFoundComponent,
+    SearchComponent,
+    WeatherComponent
   ],
   imports: [
     BrowserModule,
