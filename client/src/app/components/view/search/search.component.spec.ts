a642adbb2e9258f6c37e8a909d0638bbce8f70a3
Joshua Clarke <josh.clarke11@gmail.com>
2019-11-15 08:04:00 -0500

    test fix

--- client/src/app/components/view/search/search.component.spec.ts
+++ client/src/app/components/view/search/search.component.spec.ts
@@ -5,6 +5,7 @@ import { ReactiveFormsModule } from '@angular/forms'
 import { SearchComponent } from './search.component'
 import { TextInputComponent } from './../../element/text-input/text-input.component'
 import { CardComponent } from "../../element/weather-card/weather-card.component";
+import { UnitSelectorComponent } from '../../element/unit-selector/unit-selector.component'
 
 describe('SearchComponent', () => {
   let component: SearchComponent
@@ -13,7 +14,7 @@ describe('SearchComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, ReactiveFormsModule],
-      declarations: [SearchComponent, TextInputComponent, CardComponent]
+      declarations: [SearchComponent, TextInputComponent, CardComponent, UnitSelectorComponent]
     }).compileComponents()
   }))
 
